3755aa3f9db71e42b220c1618849052f0c82bab0
Brian Lamb <fusani.applications@gmail.com>
2014-10-21 03:12:11 +0000

    Added missing required classes

--- validate.js
+++ validate.js
@@ -1,8 +1,19 @@
 $(function () {
     // assume for all submitted forms with the validate class
     $('form.validate-me').on('submit', function() {
+        var valid = true;
+
+        // reset the form
+        $('.form-group').removeClass('has-error');
+
         // anything that's required needs something in it
-        $.each($(this).children('.validate'), function (index, value) {
+        $.each($(this).find('.validate-required'), function (index, value) {
+            if ($(value).val() == '') {
+                $(this).closest('.form-group').addClass('has-error');
+                valid = false;
+            }
         });
+
+        return false;
     });
 });
